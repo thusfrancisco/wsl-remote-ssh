73f0762f04ca4920ba43da1f181087c8479672a4
Francisco A. B. Sampaio <franciscoabsampaio@gmail.com>
2023-10-05 15:13:29 +0100

    ps1 script

---
+++ setup.ps1
@@ -0,0 +1,8 @@
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\WINDOWS\System32\bash.exe" -PropertyType String -Force
+
+
+Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
+
+Get-Service -Name *ssh*
